48ff3036b5193d3fb91340e26c9fe7f260fca3e8
allenGift <raju.allen1888@gmail.com>
2015-10-05 00:33:10 +0530

    Latest Code

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ var mysql = require('mysql'),
   connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-//	password: 'h@shl000p',
-    password: '',
+	password: 'h@shl000p',
+//    password: '',
     database: 'hashlooop',
     port: 3306
   });
